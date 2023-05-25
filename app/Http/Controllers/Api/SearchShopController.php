fda2c3c15c59df0ee2bb69207e49c3e6b3810dd1
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2023-05-25 15:11:41 +0100

    Uptimazation of the search function

--- app/Http/Controllers/Api/SearchShopController.php
+++ app/Http/Controllers/Api/SearchShopController.php
@@ -18,26 +18,49 @@ class SearchShopController extends Controller
     {
         $request->validate([
             'query' => 'required|string',
+            'city' => 'nullable|string',
+
+            'tags' => 'nullable|array',
+            'tags.*' => 'nullable|integer|exists:tags,id',
+
+            'categories' => 'nullable|array',
+            'categories.*' => 'nullable|integer|exists:categories,id',
+
         ]);
 
         $query = $request->input('query');
 
         $results = Shop::with('categories', 'tags')
-            ->where(function ($queryBuilder) use ($query) {
-                $queryBuilder->where('name', 'like', "%{$query}%")
-                    ->orWhere('description', 'like', "%{$query}%");
+            ->where(function ($queryBuilder) use ($query, $request) {
+                if ($request->filled('city')) {
+                    $queryBuilder->where('city', $request->input('city'));
+                }
+                if ($request->filled('tags')) {
+                    $queryBuilder->whereHas('tags', function ($queryBuilder) use ($request) {
+                        $queryBuilder->whereIn('id', $request->input('tags'));
+                    });
+                }
+                if ($request->filled('categories')) {
+                    $queryBuilder->whereHas('categories', function ($queryBuilder) use ($request) {
+                        $queryBuilder->whereIn('id', $request->input('categories'));
+                    });
+                }
+                $queryBuilder->where(function ($queryBuilder) use ($query) {
+                    $queryBuilder->where('name', 'like', "%{$query}%")
+                        ->orWhere('description', 'like', "%{$query}%")
+                        ->orWhereHas('categories', function ($queryBuilder) use ($query) {
+                            $queryBuilder->where('name', 'like', "%{$query}%");
+                        })
+                        ->orWhereHas('tags', function ($queryBuilder) use ($query) {
+                            $queryBuilder->where('name', 'like', "%{$query}%");
+                        });
+                });
             })
-            ->where('is_enabled', true) // only enabled shops
-
-            ->orWhereHas('categories', function ($queryBuilder) use ($query) {
-                $queryBuilder->where('name', 'like', "%{$query}%");
-            })
-            ->orWhereHas('tags', function ($queryBuilder) use ($query) {
-                $queryBuilder->where('name', 'like', "%{$query}%");
-            })
+            ->where('is_enabled', true)
             ->orderBy('name')
             ->take(10)
             ->get();
+
 
         return response()->json([
             'message' => 'Search results',
